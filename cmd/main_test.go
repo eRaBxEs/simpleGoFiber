f1b1f23762250fc94dbd7ad59c09c97f94068977
Henry Erabor <ehiojee@yahoo.com>
2022-04-28 23:27:02 +0100

    others

---
+++ cmd/main_test.go
@@ -0,0 +1,51 @@
+package main_test
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/eRaBxEs/go-fiber-api/pkg/books"
+	"github.com/eRaBxEs/go-fiber-api/pkg/common/config"
+	"github.com/eRaBxEs/go-fiber-api/pkg/common/db"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+func TestMain(m *testing.M) {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	db := db.Init(c.DBUrl)
+	app := fiber.New()
+
+	ensureTableExists(db)
+	code := m.Run()
+	clearTable(db)
+	books.RegisterRoutes(app, db)
+
+	os.Exit(code)
+}
+
+func ensureTableExists(db *gorm.DB) {
+	if err := db.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func clearTable(db *gorm.DB) {
+	db.Exec("DELETE FROM books")
+	db.Exec("ALTER SEQUENCE books_id_seq RESTART WITH 1")
+}
+
+const tableCreationQuery = `CREATE TABLE IF NOT EXISTS books
+(
+    id SERIAL,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+	description TEXT NOT NULL,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+)`
