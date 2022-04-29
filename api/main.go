ad76797b5efd4ffe43fd09692ef62cfb2544a1db
Henry Erabor <ehiojee@yahoo.com>
2022-04-29 07:41:47 +0100

    all utils and others

--- api/main.go
+++ api/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fiber-stats/bookroute"
 	"fiber-stats/util"
 	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
@@ -10,21 +14,24 @@ func main() {
 	data := util.Data{}
 	_, err := data.LoadConfiguration("util/config.json")
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Fatalln("Failed at config", err.Error())
 	}
 
-	dataString := data.GetString("hosts")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	dataInt := data.GetInt("port")
+	port := data.GetInt("port")
 
-	dataString2 := data.GetString("database.user")
+	dbUser := data.GetString("database.user")
+	dbPass := data.GetString("database.password")
+	dbHost := data.GetString("database.host")
+	dbPort := data.GetString("database.port")
+	dbName := data.GetString("database.dbname")
 
-	dataString3 := data.GetInt("database.port")
+	connectionStringURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName)
 
-	fmt.Println("dataString:", dataString)
-	fmt.Println("dataInt:", dataInt)
-	fmt.Println("dataString2:", dataString2)
-	fmt.Println("dataString3:", dataString3)
+	app := fiber.New()
+	db := util.Init(connectionStringURL)
+
+	bookroute.RegisterRoutes(app, db)
+
+	app.Listen(fmt.Sprintf(":%d", port))
+
 }
---
+++ api/util/db.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"log"
+
+	"fiber-stats/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Init(url string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&models.Book{})
+
+	return db
+}
